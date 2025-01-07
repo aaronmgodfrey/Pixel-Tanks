40fd798a278cd9b3f16caa64fd94cbb4d076ac6e
aaronmgodfrey <aaronmgodfrey@outlook.com>
2025-01-07 10:10:59 -0700

    Minor Improvements

--- public/js/Menu.js
+++ public/js/Menu.js
@@ -1,11 +1,11 @@
 class Menu {
   static scaler = document.createElement('CANVAS');
   constructor(data, id) {
+    this.elements = [];
+    this.loaded = false;
     this.id = id;
     this.listeners = data.listeners;
     this.buttons = data.buttons;
-    this.elements = [];
-    this.loaded = false;
     this.cdraw = data.cdraw.bind(this);
     for (const l in this.listeners) this.listeners[l] = this.listeners[l].bind(this);
   }
@@ -14,7 +14,7 @@ class Menu {
     this.elements = this.elements.reduce((a, c) => (c.tagName !== 'button' ? a.concat(c) : a), []);
     for (const b of this.buttons) {
       let button = document.createElement('BUTTON'), image = document.createElement('IMG');
-      button.onclick = () => (typeof b[4] === 'function' ? b[4].bind(this)() : Menus.trigger(b[4]));
+      button.onclick = () => (typeof b[4] === 'function' ? b[4]() : Menus.trigger(b[4]));
       button.width = image.width = window.innerHeight*(button.w = Menu.scaler.width = b[2])/1000;
       button.height = image.height = window.innerHeight*(button.h = Menu.scaler.height = b[3])/1000;
       const leftOffset = (window.innerWidth-window.innerHeight*1.6)/2+(button.x = b[0])/1000*window.innerHeight;
