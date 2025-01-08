dba26fb2f5c1bd6dfeda1bb99d1d4575ec7e04a8
aaronmgodfrey <aaronmgodfrey@outlook.com>
2025-01-08 09:55:25 -0700

    Delay

--- multiplayer.js
+++ multiplayer.js
@@ -150,11 +150,10 @@ class Multiplayer extends Engine {
 
   send(t) {
     if (t.busy) return t.delayed = true;
-    if (t.lastSend) {
-      if (t.lastSend+1000/settings.upsl < Date.now()) {
-        clearTimeout(t.sendTimer);
-        t.sendTimer = setTimeout(() => this.send(t), (t.lastSend+1000/settings.upsl)-Date.now());
-      } else return t.delayed = true;
+    if (t.lastSend && t.lastSend+1000/settings.upsl >= Date.now()) {
+      clearTimeout(t.sendTimer);
+      t.sendTimer = setTimeout(() => this.send(t), (t.lastSend+1000/settings.upsl)-Date.now());
+      return t.delayed = true;
     }
     t.msg.logs = this.logs.slice(t.logs).concat(t.privateLogs);
     t.msg.tickspeed = tickspeed;
