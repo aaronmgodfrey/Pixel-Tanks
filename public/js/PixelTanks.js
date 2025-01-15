1bc1f9dc65dc4b2ef59243db1eabc26d075bf500
aaronmgodfrey <aaronmgodfrey@outlook.com>
2025-01-15 09:16:08 -0700

    Fix Death Effect Rendering

--- public/js/PixelTanks.js
+++ public/js/PixelTanks.js
@@ -240,7 +240,7 @@ class PixelTanks {
           if (!this.time) this.time = Date.now(); // animatino :)
           if (this.reward) {
             GUI.clear();
-            if (this.reward[1]) GUI.drawImage(this.reward[0], 600, 400, 400, 400, 1, 0, 0, 0, 0, undefined, (Math.floor((Date.now()-this.time)/PixelTanks.images[['cosmetics', 'deathEffects'][type]][this.reward[3]+'_'].speed)%PixelTanks.images[name][this.reward[3]+'_'].frames)*200, 0, 200, 200); else GUI.drawImage(this.reward[0], 600, 400, 400, 400, 1);
+            if (this.reward[1]) GUI.drawImage(this.reward[0], 600, 400, 400, 400, 1, 0, 0, 0, 0, undefined, (Math.floor((Date.now()-this.time)/PixelTanks.images[['cosmetics', 'deathEffects'][this.reward[1]]][this.reward[3]+'_'].speed)%PixelTanks.images[['cosmetics', 'deathEffects'][this.reward[1]]][this.reward[3]+'_'].frames)*200, 0, 200, 200); else GUI.drawImage(this.reward[0], 600, 400, 400, 400, 1);
             GUI.drawText('You Got', 800, 200, 100, '#ffffff', 0.5);
             GUI.drawText(this.reward[4], 800, 800, 50, '#ffffff', 0.5);
             return GUI.drawText(this.reward[2], 800, 900, 30, {mythic: '#FF0000', legendary: '#FFFF00', epic: '#A020F0', rare: '#0000FF', uncommon: '#32CD32', common: '#FFFFFF'}[this.reward[2]], 0.5);
