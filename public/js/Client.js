e1173434bd3a7db4248cef0ac7137c0dabc62b4f
aaronmgodfrey <aaronmgodfrey@outlook.com>
2025-01-21 08:55:22 -0700

    aslkndnflKSAN:FALKfs

--- public/js/Client.js
+++ public/js/Client.js
@@ -50,7 +50,7 @@ class Client {
     this.multiplayer = multiplayer;
     this.gamemode = gamemode;
     this.ip = this.multiplayer ? ip.split('#')[0] : ''; // maybe move ip to connect??
-    if (ip.includes('#')) this.room = ip.split('#')[1];
+    if (this.multiplayer && ip.includes('#')) this.room = ip.split('#')[1];
     this.left = this.up = null;
     this.lastUpdate = {};
     this.speed = 4;
