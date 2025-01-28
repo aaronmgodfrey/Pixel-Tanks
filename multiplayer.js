1e992f28badbf61dc4eee79a79baf818e26a24f3
aaronmgodfrey <aaronmgodfrey@outlook.com>
2025-01-28 12:17:17 -0700

    Don't touch commands

--- multiplayer.js
+++ multiplayer.js
@@ -764,8 +764,15 @@ const Commands = {
   ai: [Object, 2, 7, (data, socket, server) => {
     for (let i = 0; i < Number(data[5]); i++) A.template('AI').init(Math.floor(Number(data[1]) / 100) * 100 + 10, Math.floor(Number(data[2]) / 100) * 100 + 10, Number(data[3]), Math.min(20, Math.max(0, Number(data[4]))), data[6], server);
   }],
-  crate: [FFA, 2, -1, (data, socket, server) => {
-    for (let y = 0; y < 60; y++) for (let x = 0; x < 60; x++) server.b.push(A.template('Block').init(x*100, y*100, 'crate', undefined, server));
+  nuke: [FFA, 2, -1, (data, socket, server) => {
+    for (let y = 0; y < 20; y++) for (let x = 0; x < 20; x++) {
+      for (let i = -2; i <= 2; i++) {
+        const airstrike = A.template('Block');
+        airstrike.timer = i/5+5;
+        airstrike.init(x+i*Math.cos(Math.PI*rotation/180)*100, y+i*Math.sin(Math.PI*rotation/180)*100, 'airstrike', ':', this);
+        server.b.push(airstrike);
+      }
+    }
   }],
   spectate: [Object, 3, 2, data => {
     for (const server of Object.values(servers)) for (const t of server.pt) if (t.username === data[1]) t.ded = true;
