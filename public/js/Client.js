754ad2db81dd23e4b5fc6d5ffdf3406351fc2102
aaronmgodfrey <aaronmgodfrey@outlook.com>
2025-02-05 13:51:08 -0700

    Play music on gameplay

--- public/js/Client.js
+++ public/js/Client.js
@@ -42,6 +42,7 @@ class Client {
     this.listeners = ['keydown', 'keyup', 'mousemove', 'mousedown', 'mouseup'];
   }
   constructor(ip, multiplayer, gamemode) {
+    PixelTanks.playSound('battlegrounds', 0);
     this.xp = this.crates = this.kills = this.coins = this._ops = this._ups = this._fps = this.debugMode = 0;
     this.zone = 'battlegrounds';
     this.tank = {use: [], fire: [], r: 0, baseRotation: 0};
