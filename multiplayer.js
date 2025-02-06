5038f7af558c013e6457c967cc90fea260ccd782
LostKing <jon441491@gmail.com>
2025-02-06 11:48:16 -0700

    ded'd

--- multiplayer.js
+++ multiplayer.js
@@ -769,7 +769,7 @@ const Commands = {
   ai: [Object, 2, 7, (data, socket, server) => {
     for (let i = 0; i < Number(data[5]); i++) A.template('AI').init(Math.floor(Number(data[1]) / 100) * 100 + 10, Math.floor(Number(data[2]) / 100) * 100 + 10, Number(data[3]), Math.min(20, Math.max(0, Number(data[4]))), data[6], server);
   }],
-  spectate: [Object, 3, 2, data => {
+  ded: [Object, 3, 2, data => {
     for (const server of Object.values(servers)) for (const t of server.pt) if (t.username === data[1]) t.ded = true;
   }],
   live: [Object, 2, -1, (data, socket) => {
@@ -950,7 +950,7 @@ discord.on('messageCreate', m => {
   if (m.content.startsWith('/')) {
     m.delete(1000);
     // potential freeze, unfreeze, gpts, newmap, killai, ai, swrite, twrite
-    const discordAcceptable = ['admin', 'removeadmin', 'vip', 'removevip', 'reload', 'playerlist', 'msg', 'filter', 'allow', 'run', 'ban', 'banlist', 'pardon', 'mute', 'unmute', 'kick', 'kill', 'spectate', 'live', 'restart', 'flushlogs', 'getlogs', 'announce', 'lockchat', 'lockdown', ];
+    const discordAcceptable = ['admin', 'removeadmin', 'vip', 'removevip', 'reload', 'playerlist', 'msg', 'filter', 'allow', 'run', 'ban', 'banlist', 'pardon', 'mute', 'unmute', 'kick', 'kill', 'ded', 'live', 'restart', 'flushlogs', 'getlogs', 'announce', 'lockchat', 'lockdown', ];
     const socket = {username: m.author.username, send: d => {
       toDiscord(d.message);
     }}, data = m.content.replace('/', '').split(' '), t = {username: m.author.username}, logs = {push: d => {
