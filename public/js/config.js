148e3241ee43d2368a494199d59af3bd4ce520b8
BreadSpammer <bradleydunham65@gmail.com>
2025-02-06 18:08:15 -0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -86,6 +86,6 @@ window.sourceMap = {
   },
   sounds: {
     path: 'sounds',
-    load: ['battlegrounds', 'menu', 'menuopener', 'victory', 'defeat'],
+    load: ['battlegrounds', 'menu', 'menuopener', 'victory', 'defeat', 'ice'],
   },
 }
