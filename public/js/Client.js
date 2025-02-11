da5cac477a559fe03cad937cfef2dc26104eb560
aaronmgodfrey <aaronmgodfrey@outlook.com>
2025-02-10 20:11:25 -0700

    Update Client.js

--- public/js/Client.js
+++ public/js/Client.js
@@ -43,7 +43,6 @@ class Client {
   }
   constructor(ip, multiplayer, gamemode) {
     this.xp = this.crates = this.kills = this.coins = this._ops = this._ups = this._fps = this.debugMode = 0;
-    this.zone = 'battlegrounds';
     this.tank = {use: [], fire: [], r: 0, baseRotation: 0};
     this.hostupdate = {b: [], s: [], pt: [], d: [], ai: [], entities: []};
     this.paused = this.canRespawn = false;
