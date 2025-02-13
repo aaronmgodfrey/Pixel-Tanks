003456b64d0f77585929d9cfd06464c56e30a009
aaronmgodfrey <aaronmgodfrey@outlook.com>
2025-02-13 07:53:32 -0700

    AI Classes
    Stealth still buggy

--- public/js/AI.js
+++ public/js/AI.js
@@ -83,11 +83,16 @@ class AI {
       if (this.canFire) this.fireCalc(this.target.x, this.target.y);
     }
     if (this.canClass && this.mode !== 0 && Math.random() < 1/300) {
-      if (this.class === 'tactical') this.fireCalc(this.target.x, this.target.y, 'megamissle'); // 25
-      if (this.class === 'stealth') this.host.useAbility(this, 'invis'); // 40
-      if (this.class === 'builder') this.host.useAbility(this, 'turret'); // 20
-      if (this.class === 'warrior') this.host.useAbility(this, 'buff'); // 40
-      if (this.class === 'medic') this.host.useAbility(this, 'healwave'); // greedy self-heal :D // 30
+      if (this.class === 'tactical') this.fireCalc(this.target.x, this.target.y, 'megamissle');
+      if (this.class === 'stealth') this.host.useAbility(this, 'invis');
+      if (this.class === 'builder') this.host.useAbility(this, 'turret');
+      if (this.class === 'warrior') {
+        this.canBoost = false;
+        this.immune = Date.now();
+        setTimeout(() => (this.canBoost = true), 5000);
+        this.host.useAbility(this, 'bash');
+      }
+      if (this.class === 'medic') this.host.useAbility(this, 'healburst');
       if (this.class === 'fire') for (let i = -30, len = 30; i < len; i += 5) A.template('Shot').init(this.x+40, this.y+40, 70, this.r+90+i, 'fire', this.team, this.rank, this.host);
       this.canClass = false;
       setTimeout(() => (this.canClass = true), 1000*[25, 2, 30, 15, 30, 10][['tactical', 'stealth', 'builder', 'warrior', 'medic', 'fire'].indexOf(PixelTanks.userData.class)]);
