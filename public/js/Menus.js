f2e71d4a39e920d3cc63b4f2f5aec7314c6da8b4
BreadSpammer <bradleydunham65@gmail.com>
2025-02-13 15:04:20 -0700

    Update Menus.js

--- public/js/Menus.js
+++ public/js/Menus.js
@@ -427,12 +427,12 @@ class Menus {
             const classX = [701, 810, 810, 701, 810, 701], classY = [348, 348, 564, 564, 456, 456];
             for (let i = 0; i < 6; i++) {
               if (!PixelTanks.userData.classes[i]) GUI.drawImage(PixelTanks.images.menus.locked, classX[i], classY[i], 88, 88, 1);
-              GUI.drawText(['Tactical', 'Stealth', 'Warrior', 'Medic', 'Builder', 'Fire'][i], classX[i]+44, classY[i]+88, 30, '#7A7A7A', .5);
+              GUI.drawText(['Tactical', 'Stealth', 'Warrior', 'Medic', 'Builder', 'Fire'][i], classX[i]+44, classY[i]+44, 30, '#303030', .5);
             }
             GUI.draw.strokeStyle = '#FFFF00';
             GUI.draw.lineWidth = 10;
             // fix excessive if statement below
-            if (PixelTanks.userData.class === 'tactical') GUI.draw.strokeRect(701, 348, 88, 88); else if (PixelTanks.userData.class === 'fire') GUI.draw.strokeRect(701, 456, 88, 88); else if (PixelTanks.userData.class === 'medic') GUI.draw.strokeRect(701, 565, 88, 88); else if (PixelTanks.userData.class === 'stealth') GUI.draw.strokeRect(814, 348, 88, 88); else if (PixelTanks.userData.class === 'builder') GUI.draw.strokeRect(814, 456, 88, 88); else if (PixelTanks.userData.class === 'warrior') GUI.draw.strokeRect(814, 565, 88, 88);
+            if (PixelTanks.userData.class === 'tactical') GUI.draw.strokeRect(701, 348, 88, 88); else if (PixelTanks.userData.class === 'fire') GUI.draw.strokeRect(701, 456, 88, 88); else if (PixelTanks.userData.class === 'medic') GUI.draw.strokeRect(701, 565, 88, 88); else if (PixelTanks.userData.class === 'stealth') GUI.draw.strokeRect(810, 348, 88, 88); else if (PixelTanks.userData.class === 'builder') GUI.draw.strokeRect(810, 456, 88, 88); else if (PixelTanks.userData.class === 'warrior') GUI.draw.strokeRect(810, 565, 88, 88);
           } else if (this.itemTab) {
             GUI.drawImage(PixelTanks.images.menus.itemTab, 580, 334, 440, 332, 1);
             const key = {airstrike: [598, 352], super_glu: [706, 352], duck_tape: [814, 352], shield: [922, 352], flashbang: [598, 460], bomb: [706, 460], dynamite: [814, 460], usb: [922, 460], weak: [598, 568], strong: [706, 568], spike: [814, 568], reflector: [922, 568]};
