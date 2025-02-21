ce4ba2fc59a208fb94241182fdae3f9276365dc6
BreadSpammer <bradleydunham65@gmail.com>
2025-02-21 08:51:58 -0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -15,7 +15,7 @@ window.sourceMap = {
     {
       ref: 'menus',
       path: 'menus',
-      load: ['perksheet', 'world1', 'world2', 'world3', 'world4', 'world5', 'ui', 'start', 'main', 'multiplayer', 'victory', 'defeat', 'crate', 'settings', 'inventory', 'classTab', 'itemTab', 'perkTab', 'cosmeticTab', 'shop', 'shop2', 'broke', 'pause', 'locked', 'adrenaline', 'cooldown', 'core', 'radar', 'refresh', 'scavenger', 'shield', 'thermal', 'arrow', 'arrow_friendly', 'upgrader'],
+      load: ['perksheet', 'world1', 'world2', 'world3', 'world4', 'world5', 'ui', 'stats', 'start', 'main', 'multiplayer', 'victory', 'defeat', 'crate', 'settings', 'inventory', 'classTab', 'itemTab', 'perkTab', 'cosmeticTab', 'shop', 'shop2', 'broke', 'pause', 'locked', 'adrenaline', 'cooldown', 'core', 'radar', 'refresh', 'scavenger', 'shield', 'thermal', 'arrow', 'arrow_friendly', 'upgrader'],
     },
     {
       ref: 'animations',
