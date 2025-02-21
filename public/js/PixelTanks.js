5be4375d3516dcf9999eb4414442d4962930db7a
aaronmgodfrey <aaronmgodfrey@outlook.com>
2025-02-21 08:49:51 -0700

    FIX

--- public/js/PixelTanks.js
+++ public/js/PixelTanks.js
@@ -70,6 +70,8 @@ class PixelTanks {
 
   static playSound(name, type) {
     PixelTanks.sounds[name].loop = type === (PixelTanks.sounds[name].currentTime = 0);
+    if (isNaN(PixelTanks.userData.volume)) PixelTanks.userData.volume = 100;
+    if (isNaN(PixelTanks.userData.music)) PixelTanks.userData.music = 100;
     PixelTanks.sounds[name].volume = PixelTanks.userData[['battlegrounds', 'gem', 'ice', 'cave', 'deep'].includes(name) ? 'music' : 'volume']/100;
     PixelTanks.sounds[name].play();
   }
