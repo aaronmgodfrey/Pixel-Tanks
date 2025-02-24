eef3680217664c49b985bf6e9f16fc3bd410ee29
aaronmgodfrey <aaronmgodfrey@outlook.com>
2025-02-24 09:55:27 -0700

    SNDf;nsdfad

--- public/js/Tank.js
+++ public/js/Tank.js
@@ -31,7 +31,7 @@ class Tank {
     host.updateEntity(this, Tank.raw);
     host.override(this);
     host.loadCells(this, this.x, this.y, 80, 80);
-    if (data.socket) this.chunkload(host, -100000, -100000, this.x, this.y);
+    if (data.socket) host.chunkload(this, -100000, -100000, this.x, this.y);
     host.pt.push(this);
     for (const p of Tank.s) {
       this.raw[p] = this[p];
