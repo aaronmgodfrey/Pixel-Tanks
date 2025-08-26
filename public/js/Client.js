e6e71570dec245415ee4be1c5062a9fdfa495f57
BreadSpammer <bradleydunham65@gmail.com>
2025-08-26 07:51:24 -0700

    Update Client.js

--- public/js/Client.js
+++ public/js/Client.js
@@ -400,7 +400,7 @@ class Client {
         if (t.color === '#FF0000') {
           GUI.drawText(username, t.x+40, t.y-25, 50, '#ff0000', 0.5);
         } else if (t.color === '#0000FF') GUI.drawText(username, t.x+40, t.y-25, 50, '#0000ff', 0.5);
-      } else GUI.drawText(username, t.x+40, t.y-25, 50, t.username == 'cheese' ? '#ffff00' : '#ffffff', 0.5);
+      } else GUI.drawText(username, t.x+40, t.y-25, 50, t.nameColor, 0.5);
     }
     if (t.gambleCounter > 0 && t.hp <= 0 && !t.ded) GUI.drawText('Gambled! '+t.gambleCounter, t.x+40, t.y-75, 30, '#ffffff', 0.5);
     if (t.buff) GUI.drawImage(PixelTanks.images.tanks.buff, t.x-5, t.y-5, 80, 80, .2);
