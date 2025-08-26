859a9bae3a0d114d6155c7bd8643b291d1cabaf5
BreadSpammer <bradleydunham65@gmail.com>
2025-08-26 07:38:46 -0700

    -1 line of code (something was pasted twice :/)

--- public/js/Client.js
+++ public/js/Client.js
@@ -399,7 +399,6 @@ class Client {
       } else if (teamname === 'LOBBY') {
         if (t.color === '#FF0000') {
           GUI.drawText(username, t.x+40, t.y-25, 50, '#ff0000', 0.5);
-          GUI.drawText(username, t.x+40, t.y-25, 50, '#ff0000', 0.5);
         } else if (t.color === '#0000FF') GUI.drawText(username, t.x+40, t.y-25, 50, '#0000ff', 0.5);
       } else GUI.drawText(username, t.x+40, t.y-25, 50, t.username == 'cheese' ? '#ffff00' : '#ffffff', 0.5);
     }
