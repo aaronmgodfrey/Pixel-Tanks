ff97275a1e84e3c0ebf12f637a9dd28e9501274b
aaronmgodfrey <aaronmgodfrey@outlook.com>
2025-12-10 18:08:22 -0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -48,7 +48,7 @@ window.sourceMap = {
     uncommon: ['tenna', 'hazard', 'redaction', '035', 'feesh', 'knight', 'mango', 'camo', 'ripped', 'scarred', 'block', 'chip', 'deep_scratch', 'evil_eye', 'inferno_eye', 'motherboard', 'blue_wings', 'blue_horns', 'white_wings', 'white_horns', 'gold_wings', 'gold_horns', 'devil_wings', 'devil_horns', 'hazard', 'angel_wings', 'bat_wings', 'locked', 'mini_tank', 'dust', 'pouch'],
     rare: ['goldance', 'suit', 'raging_eye', 'banana', 'jason', 'the_swift_crown', 'flowers', 'fives', 'fivehalf', 'nvg', 'cracked', 'blue_tint', 'glitch', 'blue_helmet', 'white_helmet', 'helmet', 'gold_helmet', 'hacker_hoodie', 'purple', 'visor', 'veins', 'cookie', 'splattered'],
     epic: ['smoke', 'christmas_hat', 'christmas_lights', 'dizzy', 'rage', 'toxic', 'error', 'supersight', 'crown', 'darkcrown', 'ghoul'],
-    legendary: ['smooth_suit', 'EMP_burst', 'thedark', 'lostcrown', 'dragon', 'chad_face', 'plasma', 'cry', 'missing', 'hoodie', 'galaxy'],
+    legendary: ['sans', 'smooth_suit', 'EMP_burst', 'thedark', 'lostcrown', 'dragon', 'chad_face', 'plasma', 'cry', 'missing', 'hoodie', 'galaxy'],
     admin: ["void's_edge", 'smooth'],
     mythic: ['power_armor', 'lava_monster', 'terminator', 'mlg_glasses', 'corrupted', 'blood_rabbit', 'demon_goat', 'crystal_face'],
     meta: {},
